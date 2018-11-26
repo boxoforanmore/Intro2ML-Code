cebcc85071e3ba19e5bb11ab0e00f3f27d5f8c45
Peter Nielson <computergeekpete@gmail.com>
2018-11-25 21:22:13 -0700

    Added more pages

--- Ch9/flask_testing/lst_flask_app_1/app.py
+++ Ch9/flask_testing/lst_flask_app_1/app.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template, request
+from wtforms import Form, TextAreaField, validators
 
 app = Flask(__name__)
-@app.route('/')
 
+# Form class with simple validators
+class HelloForm(Form):
+    sayhello = TextAreaField('', [validators.DataRequired()])
+
+# Main page method
+@app.route('/')
 def index():
+    form = HellowForm(request.form)
     return render_template('first_app.html')
+
+# Hello page method
+@app.route('/hello', methods=['POST'])
+def hello():
+    form = HelloForm(request.form)
+    if (request.method == 'POST') and (form.validate()):
+        name = request.form['sayhello']
+        return render_template('hello.html', name=name)
+    return render_template('first_app.html', form=form)
+
 
 if __name__ == '__main__':
     app.run()
