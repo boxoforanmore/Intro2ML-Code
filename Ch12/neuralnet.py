af4e483911b0a784b912a5e415d04595704ed101
Peter Nielson <computergeekpete@gmail.com>
2018-12-01 22:14:18 -0700

    Changed line to use python int casting (runtime failed otherwise)

--- Ch12/neuralnet.py
+++ Ch12/neuralnet.py
@@ -55,7 +55,7 @@ class NeuralNetMLP(object):
         onehot : array, shape = (n_samples, n_labels)
 
         """
-        onehot = np.zeros((n_classes, y.shape[0].astype(int)))
+        onehot = np.zeros((n_classes, int(y.shape[0])))#.astype(int)))
         for idx, val in enumerate(y):
             onehot[val, idx] = 1.
         return onehot.T
