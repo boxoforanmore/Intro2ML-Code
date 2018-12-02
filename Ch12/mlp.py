76d6beb4f227ccba97642ca1975a5b5416f88c07
Peter Nielson <computergeekpete@gmail.com>
2018-12-01 22:19:57 -0700

    Added plot for training vs validation accuracy

--- Ch12/mlp.py
+++ Ch12/mlp.py
@@ -1,5 +1,7 @@
 # Running an MLP nn
+import numpy as np
 from neuralnet import NeuralNetMLP
+
 
 mnist = np.load('mnist_scaled.npz')
 
@@ -9,11 +11,23 @@ nn = NeuralNetMLP(n_hidden=100, l2=0.01, epochs=200, eta=0.0005,
                   minibatch_size=100, shuffle=True, seed=1)
 
 print(nn.fit(X_train=X_train[:55000], y_train=y_train[:55000],
-             X_test=X_test[55000:], y_test=y_test[55000:]))
+             X_valid=X_train[55000:], y_valid=y_train[55000:]))
 
 import matplotlib.pyplot as plt
 plt.plot(range(nn.epochs), nn.eval_['cost'])
 
+# Plots cost over 200 epochs
 plt.ylabel('Cost')
 plt.xlabel('Epochs')
 plt.show()
+
+
+# Plot training and validation accuracy
+plt.plot(range(nn.epochs), nn.eval_['train_acc'], label='training')
+plt.plot(range(nn.epochs), nn.eval_['valid_acc'], label='validation', linestyle='--')
+
+plt.ylabel('Accuracy')
+plt.xlabel('Epochs')
+plt.legend()
+plt.show()
+
