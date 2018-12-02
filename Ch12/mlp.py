118cc1ac50dbdfc9cd5142bd3ef60ed594f4597f
Peter Nielson <computergeekpete@gmail.com>
2018-12-02 14:20:13 -0700

    Added some graphing code and changed the l2 and epochs to better train on the dataset

--- Ch12/mlp.py
+++ Ch12/mlp.py
@@ -7,7 +7,8 @@ mnist = np.load('mnist_scaled.npz')
 
 X_train, y_train, X_test, y_test = [mnist[f] for f in mnist.files]
 
-nn = NeuralNetMLP(n_hidden=100, l2=0.01, epochs=200, eta=0.0005,
+# l2=0.01 overfits the data
+nn = NeuralNetMLP(n_hidden=100, l2=0.1, epochs=250, eta=0.0005,
                   minibatch_size=100, shuffle=True, seed=1)
 
 print(nn.fit(X_train=X_train[:55000], y_train=y_train[:55000],
@@ -31,3 +32,32 @@ plt.xlabel('Epochs')
 plt.legend()
 plt.show()
 
+
+# Evaluate generalization performance by calculating prediction accuracy on test set
+y_test_pred = nn.predict(X_test)
+acc = (np.sum(y_test == y_test_pred).astype(np.float) / X_test.shape[0])
+
+print()
+print('Training accuracy: %.2f%%' % (acc * 100))
+print()
+
+
+# Looking at the images the MLP struggles with:
+miscl_img = X_test[y_test != y_test_pred][:25]
+correct_lab = y_test[y_test != y_test_pred][:25]
+miscl_lab = y_test_pred[y_test_pred][:25]
+
+fig, ax = plt.subplots(nrows=5, ncols=5, sharex=True, sharey=True)
+
+ax = ax.flatten()
+
+for i in range(25):
+    img = miscl_img[i].reshape(28, 28)
+    ax[i].imshow(img, cmap='Greys', interpolation='nearest')
+
+    ax[i].set_title('%d) t: %d p: %d' % (i+1, correct_lab[i], miscl_lab[i]))
+
+ax[0].set_xticks([])
+ax[0].set_yticks([])
+plt.tight_layout()
+plt.show()
