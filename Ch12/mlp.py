03fb1bcd234aea4a18890522f27164e4c50c48ae
Peter Nielson <computergeekpete@gmail.com>
2018-12-01 21:32:19 -0700

    Added representative plot of each digit from nn

--- Ch12/mlp.py
+++ Ch12/mlp.py
@@ -55,3 +55,19 @@ print('Rows: %d, columns: %d' % (X_test.shape[0], X_test.shape[1]))
 print()
 
 
+# Visualize examples of the digits 0-9 after reshaping the 784-pixel vectors
+# from dfeature matrix into original 28x28 image
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots(nrows=2, ncols=5, sharex=True, sharey=True)
+
+ax = ax.flatten()
+
+for i in range(10):
+    img = X_train[y_train == i][0].reshape(28, 28)
+    ax[i].imshow(img, cmap='Greys')
+
+ax[0].set_xticks([])
+ax[0].set_yticks([])
+plt.tight_layout()
+plt.show()
